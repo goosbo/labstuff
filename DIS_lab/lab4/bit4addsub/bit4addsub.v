7b54d19469d70b0fcd65cb35b24906d9ff0bcf97
goosbo <keb1898@protonmail.com>
2025-08-29 03:00:52 +0530

    fixed a mistake with subtraction carry

--- DIS_lab/lab4/bit4addsub/bit4addsub.v
+++ DIS_lab/lab4/bit4addsub/bit4addsub.v
@@ -7,12 +7,13 @@ module bit4addsub(control, x, y, s, cout);
     wire [3:0] y_xor;
     assign y_xor = y ^ {4{control}};
 
-    wire c0, c1, c2;
+    wire c0, c1, c2,c3;
 
     fulladder fa0(control, x[0], y_xor[0], s[0], c0);
     fulladder fa1(c0, x[1], y_xor[1], s[1], c1);
     fulladder fa2(c1, x[2], y_xor[2], s[2], c2);
-    fulladder fa3(c2, x[3], y_xor[3], s[3], cout);
+    fulladder fa3(c2, x[3], y_xor[3], s[3], c3);
+    assign cout = control? ~c3: c3;
 endmodule
 
 module fulladder(cin,x,y,s,cout);
