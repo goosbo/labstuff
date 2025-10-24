3a7d70aa3c7ed2201cc56bd8e088bb0bb0f6c447
goosbo <keb1898@protonmail.com>
2025-10-24 11:26:35 +0530

    ee

--- DIS_lab/lab8/shiftregister/shiftregister.v
+++ DIS_lab/lab8/shiftregister/shiftregister.v
@@ -2,14 +2,14 @@
 module shiftregister(I, CLK, Q);
     input I, CLK;
     output [5:0] Q;
-    wire [5:0] dff_out;
+    wire [5:0] D;
 
-    dflipflop dff0 (I, CLK, dff_out[0]);
-    dflipflop dff1 (dff_out[0], CLK, dff_out[1]);
-    dflipflop dff2 (dff_out[1], CLK, dff_out[2]);
-    dflipflop dff3 (dff_out[2], CLK, dff_out[3]);
-    dflipflop dff4 (dff_out[3], CLK, dff_out[4]);
-    dflipflop dff5 (dff_out[4], CLK, dff_out[5]);
+    dflipflop dff0 (I, CLK, D[0]);
+    dflipflop dff1 (D[0], CLK, D[1]);
+    dflipflop dff2 (D[1], CLK, D[2]);
+    dflipflop dff3 (D[2], CLK, D[3]);
+    dflipflop dff4 (D[3], CLK, D[4]);
+    dflipflop dff5 (D[4], CLK, D[5]);
 
-    assign Q = dff_out;
+    assign Q = D;
 endmodule
